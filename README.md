1c036337c3f57c9d43f38279c81ac90a6a27edc9
Da532 <ewanhaynes@gmail.com>
2018-03-28 03:32:44 +0100

    First upload
    If there are bugs, I will squash them later. My bed is calling my name.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # DKeyword
 🤚 - An easy way to pick up upon, respond to or block keywords in Discord.
+
+This is more or less a rewritten version of my old Theresa May selfbot. It's a lot more streamlined, cleaner and better in every way than the former.
+
+### Setup
+Download the files for the bot and make sure Python 3.6+ is installed.
+After this, simply install the following dependencies:
+- discord.py
+
+To complete the setup, open the bot file in an IDE or text editor and input your token, desired banned words and desired words to respond to.
+You can enable and disable these two features at ease.
---
+++ bot.py
@@ -0,0 +1,71 @@
+# DKeyword by Da532.
+
+#---CONFIG---
+enabled_banned_words = True # Change to False if you want this disabled.
+enabled_responses = True # Change to False if you want this disabled.
+banned_words = ("naughty, word, xd, nigeria") # Write words here you'd like to be removed.
+responses = {"hi" : "Hello!", "jeff" : "Jeff is bad!!"} # Write words and responses to them here.
+token = "TOKEN_HERE" # Your bot token here.
+
+#---BOT---
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+bot = commands.Bot(command_prefix="DKeyword")
+bot.remove_command("help")
+
+info = "[Info] "
+error = "[Error] "
+success = "[Success] "
+
+@bot.event
+async def on_ready():
+    print (info + "Ready!") 
+
+async def detection(message):
+    if bot.user.id == message.author.id:
+        return
+    words = message.content.lower().split(" ")
+    for word in words:
+        if not message.channel.is_private:
+            if word in banned_words.lower():
+                if enabled_banned_words == False:
+                    return
+                if len(word) == 1:
+                    pass
+                else:
+                    try:
+                        await bot.delete_message(message)
+                        print(success + "{} has posted a banned word in {}. It was successfully removed from chat.".format(message.author.name, message.server.name))
+                    except:
+                        print(error + "{} has posted a banned word in {}. It failed to be removed from chat.".format(message.author.name, message.server.name))
+                        pass
+                    try:
+                        await bot.send_message(message.channel, ":x: That word is not allowed here!")
+                        return
+                    except:
+                        print(error + "I have failed to post a message in {}.".format(message.server.name))
+                        return
+
+            elif word in responses:
+                if enabled_responses == False:
+                    return
+                for key in responses:
+                    if key in word:
+                        _key = key.lower()
+                    else:
+                        pass
+                try:
+                    await bot.send_message(message.channel, responses[_key])
+                    print(success + "{} has triggered the response {} in {}.".format(message.author.name, _key, message.server.name))
+                    return
+                except:
+                    print(error + "I have failed to post a message in {}.".format(message.server.name))
+                    return
+            else:
+                pass
+
+bot.add_listener(detection, 'on_message')
+
+bot.run(token)
